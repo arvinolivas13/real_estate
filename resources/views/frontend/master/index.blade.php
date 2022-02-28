afdc4fac871b75316f20799581dbe0c0aaf704c9
judemuego <47876710+judemuego@users.noreply.github.com>
2022-03-01 03:25:33 +0800

    push

--- resources/views/frontend/master/index.blade.php
+++ resources/views/frontend/master/index.blade.php
@@ -34,7 +34,7 @@
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-
+  @yield('style')
   @yield('calendar-js')
   <script src="{{asset('frontend/js/jquery.min.js')}}"></script>
   <script src="{{asset('frontend/js/jquery-migrate-3.0.1.min.js')}}"></script>
