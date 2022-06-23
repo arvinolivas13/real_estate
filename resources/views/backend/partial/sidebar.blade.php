23f372aa74e120d8be3727a0281563b21ed9e680
Arvin Olivas <arvin.olivas15@gmail.com>
2022-06-24 03:18:09 +0800

    commit change

--- resources/views/backend/partial/sidebar.blade.php
+++ resources/views/backend/partial/sidebar.blade.php
@@ -85,6 +85,18 @@
                 </ul>
             </li>
             @endrole
+            @role('Super Admin')
+            <li class="sidebar-header">
+                Other
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed">
+                    <span class="item">
+                        <i class="align-middle mr-2 fas fa-fw fa-receipt"></i> <span class="align-middle">Document Management</span>
+                    </span> 
+                </a>
+            </li>
+            @endrole
 
             {{-- <li class="sidebar-header">
                 Maintenance
