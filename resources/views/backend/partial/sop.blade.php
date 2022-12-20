09b3f247e040a341a2ecfbd6c527bc300bd2caac
judemuego <47876710+judemuego@users.noreply.github.com>
2022-12-20 15:16:16 +0800

    sop

--- resources/views/backend/partial/sop.blade.php
+++ resources/views/backend/partial/sop.blade.php
@@ -6,6 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="{{{ URL::asset('backend/css/modern.css') }}}" rel="stylesheet">
+    <style>
+        table {
+          font-family: arial, sans-serif;
+          border-collapse: collapse;
+          width: 100%;
+          color: black;
+        }
+        
+        td, th {
+          border: 1px solid #b1ff7eb6;
+          text-align: left;
+          padding: 8px;
+        }
+        
+        tr:nth-child(even) {
+          background-color: #b1ff7eb6;
+        }
+    </style>
 
 </head>
 <body>
@@ -25,36 +43,158 @@
         </div>
         <div style="padding: 1em; padding: 1em; border-bottom: 3px solid #4aa40f;"></div>
         <div style="padding-top: 2em;" class="">
-            <div class="contract-title" style="color:black; text-align: center; font-size: 32px; text-transform: uppercase; font-weight: bold;">Schedule of Payment</div>
+            <div class="contract-title" style="color:black; text-align: center; font-size: 32px; text-transform: uppercase; font-weight: bold;">Grand Villas  Farm - East</div>
+            <div class="contract-title" style="color:red; text-align: center; font-size: 22px; text-transform: uppercase; font-weight: bold;">Schedule of Payment</div>
         </div>
         <div style="padding: 2em;"></div>
         <div class="row">
-            <div class="col" style="font-size: 18px; text-transform: uppercase; color: black;">
-                Known all men by these presents:
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Client : <span style="float: right; color: black;">Juan Dela Cruz</span>
+            </div>
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Term : <span style="float: right; color: black;">48 Months</span>
             </div>
         </div>
-        <div style="padding: 1em;"></div>
+        <div class="row">
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Phase : <span style="float: right; color: black;">1</span>
+            </div>
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Financing : <span style="float: right; color: black;">In-House</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Block : <span style="float: right; color: black;">0</span>
+            </div>
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Interest : <span style="float: right; color: black;">0%</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Lot No : <span style="float: right; color: black;">0</span>
+            </div>
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                TCP : <span style="float: right; color: black;">396, 900.00</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Lot Area in SQM : <span style="float: right; color: black;">105</span>
+            </div>
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Reservation : <span style="float: right; color: black;">19,845.00</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Due Date : <span style="float: right; color: black;">Every 27th of the month</span>
+            </div>
+            <div class="col-6" style="font-size: 15px; text-transform: uppercase; color: #626262; font-weight:bold;">
+                Balance : <span style="float: right; color: black;">377, 055.00</span>
+            </div>
+        </div>
 
-        <div class="row">
-            <div class="col" style="font-size: 18px; color: black;">
-                <p style="text-indent: 60px;">This CONTRACT TO SELL, executed and entered into by and between:</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col" style="font-size: 18px; color: black;">
-                <p style="text-indent: 60px;"><span style="font-weight: bold; text-transform: uppercase;">Brilliant Five J Construction & Development Corporation</span>, a company duly organized and existing under Philippine Laws, with business office address at #2D RNL Building, Brgy. Bagumbayan, Teresa Rizal, and herein represented in this act by <span style="font-weight: bold; text-transform: uppercase;"> Ms. Felipa De Guzman-Obeña</span>, herein after referred to as the <span style="font-weight: bold; text-transform: uppercase;">first Party-Seller;</span> and</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col" style="font-size: 18px; color: black;">
-                <p style="text-indent: 60px;"><span style="font-weight: bold; text-transform: uppercase;">Liezel D. Hidalgo</span>, of legal age, single, Filipino and with residence and postal address at 146 St. Jude St. Circle St. St Claire 3, Conception, Marikina City, herein after referred to as the  <span style="font-weight: bold; text-transform: uppercase;"> Ms. Felipa De Guzman-Obeña</span>, herein after referred to as the <span style="font-weight: bold; text-transform: uppercase;">second-party buyer</span></p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col" style="font-size: 18px; color: black;">
-                <p style="text-indent: 60px;">IN WITNESS WHEREOF, We have hereunto set our hands this day of <span style="font-weight: bold; text-decoration: underline;">MAY 07, 2021</span> in the municipality of Teresa Rizal</p>
-            </div>
-        </div>
+        <div style="padding: 2em;"></div>
+
+        <table>
+            <tr>
+              <th>Due</th>
+              <th>Monthly Due</th>
+              <th>Principal</th>
+              <th>Balance</th>
+              <th>Remarks</th>
+            </tr>
+            <tr>
+                <td>January 27, 2021</td>
+                <td>1st</td>
+                <td>7,855.00</td>
+                <td>369,200.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>February 27, 2021</td>
+                <td>2nd</td>
+                <td>7,855.00</td>
+                <td>361,345.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>March 27, 2021</td>
+                <td>3rd</td>
+                <td>7,855.00</td>
+                <td>353,490.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>April 27, 2021</td>
+                <td>4th</td>
+                <td>7,855.00</td>
+                <td>345,635.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>May 27, 2021</td>
+                <td>5th</td>
+                <td>7,855.00</td>
+                <td>369,200.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>June 27, 2021</td>
+                <td>6th</td>
+                <td>7,855.00</td>
+                <td>361,345.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>July 27, 2021</td>
+                <td>7th</td>
+                <td>7,855.00</td>
+                <td>353,490.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>August 27, 2021</td>
+                <td>8th</td>
+                <td>7,855.00</td>
+                <td>345,635.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>September 27, 2021</td>
+                <td>9th</td>
+                <td>7,855.00</td>
+                <td>369,200.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>October 27, 2021</td>
+                <td>10th</td>
+                <td>7,855.00</td>
+                <td>361,345.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>November 27, 2021</td>
+                <td>11th</td>
+                <td>7,855.00</td>
+                <td>353,490.00</td>
+                <td>None</td>
+            </tr>
+            <tr>
+                <td>December 27, 2021</td>
+                <td>12th</td>
+                <td>7,855.00</td>
+                <td>345,635.00</td>
+                <td>None</td>
+            </tr>
+                  
+          </table>
+
+
+        
         <div style="padding: 3em;"></div>
 
         <div class="row">
