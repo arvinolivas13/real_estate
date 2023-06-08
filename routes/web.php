db1e85343375412086a643660f2ae696acf8e2e9
Arvin Olivas <arvin.olivas15@gmail.com>
2023-06-08 09:33:23 +0800

    Attachment

--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,7 @@ Route::group(['middleware' => ['auth']], function() {
         Route::get          ('/amortization/{code}',         'PaymentController@ma_counter'                     )->name('edit');
         Route::get          ('/get',                         'PaymentController@get'                            )->name('get');
         Route::post         ('/filter',                      'PaymentController@filter'                         )->name('filter');
+        Route::get          ('/get_attachment/{id}',         'PaymentController@getAttachment'                  )->name('filter');
         Route::post         ('/update/{id}',                 'PaymentController@update'                         )->name('update');
         Route::get          ('/destroy/{id}',                'PaymentController@destroy'                        )->name('delete');
         Route::get          ('/lot/{id}',                    'PaymentController@lotNo'                          )->name('delete');
